f91d0ae261b3e0d64f2a5275176e5b73d97f4c5a
utkal-Nayak <nayakutkal05@gmail.com>
2025-11-17 10:58:49 +0530

    push6

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -11,6 +11,31 @@ sys.path.append(src_path)
 # Import the add function
 from math_utils import add
 
+
 # Test function
-def test_addition():
+def test_add_positive():
     assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-2, -5) == -7
+
+def test_add_mixed_numbers():
+    assert add(-2, 5) == 3
+
+def test_add_zero():
+    assert add(0, 5) == 5
+
+# -------------------------------
+# Test cases for sub() function
+# -------------------------------
+def test_sub_positive():
+    assert sub(5, 2) == 3
+
+def test_sub_negative_result():
+    assert sub(2, 5) == -3
+
+def test_sub_with_negative_numbers():
+    assert sub(-5, -2) == -3
+
+def test_sub_zero():
+    assert sub(5, 0) == 5
